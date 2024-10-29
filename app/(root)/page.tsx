c8b9657d4090343d1298d2cd22c82914336bf9be
Rasel Hossen <raselhossen052@gmail.com>
2024-10-29 17:07:30 +0600

    implement-search-form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,11 @@
-export default function Home() {
+import SearchForm from '@/components/SearchForm';
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
   return (
     <>
       <section className='pink_container'>
@@ -6,7 +13,10 @@ export default function Home() {
           Pitch Your startup <br />
           Content with entrepreneurs
         </h1>
-        <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions</p>
+        <p className='sub-heading !max-w-3xl'>
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
+        </p>
+        <SearchForm query={query} />
       </section>
     </>
   );
---
+++ components/SearchFormReset.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { X } from 'lucide-react';
+import Link from 'next/link';
+
+export default function SearchFormReset() {
+  function reset() {
+    const form = document.querySelector('.search-form') as HTMLFormElement;
+    if (form) form.reset();
+  }
+  return (
+    <button
+      type='reset'
+      onClick={reset}
+    >
+      <Link
+        href='/'
+        className='search-btn text-white'
+      >
+        <X className='size-5' />
+      </Link>
+    </button>
+  );
+}
