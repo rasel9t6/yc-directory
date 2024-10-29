aabec982df6d509eaf22aec7115eda36fa812353
Rasel Hossen <raselhossen052@gmail.com>
2024-10-29 16:33:16 +0600

    implement-shadcn-ui

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,13 @@
-
 export default function Home() {
   return (
     <>
-      Hello
+      <section className='pink_container'>
+        <h1 className='heading'>
+          Pitch Your startup <br />
+          Content with entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions</p>
+      </section>
     </>
   );
 }
